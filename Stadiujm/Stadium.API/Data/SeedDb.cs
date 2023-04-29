92803bdffc6479eeed16e635332a85dfec944314
JFelipe115 <netsoft00@gmail.com>
2023-04-28 19:54:53 -0500

    Create project and database

---
+++ Stadiujm/Stadium.API/Data/SeedDb.cs
@@ -0,0 +1,32 @@
+﻿using Stadium.Shared.Entities;
+
+namespace Stadium.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckTicketsAsync();
+        }
+
+        private async Task CheckTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 1; i <= 50000; i++)
+                {
+                    _context.Tickets.Add(new Ticket { Id = i, DateUse = null, WasUsed = false, Entrance = null });
+                }
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Stadiujm/Stadium.Shared/Entities/Ticket.cs
@@ -0,0 +1,13 @@
+﻿namespace Stadium.Shared.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public DateTime? DateUse { get; set; }
+
+        public bool WasUsed { get; set; }
+
+        public string? Entrance { get; set; }
+    }
+}
